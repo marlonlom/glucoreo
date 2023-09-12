6bda1a63767584ec389fcc8fac228a022c695ddc
Marlon López <malm.gm@gmail.com>
2023-09-12 14:45:20 -0500

    feat: initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  @Suppress("UnstableApiUsage")
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "glucoreo"
+include(":apps:mobile_app")
+include(":libraries:core-converter")
